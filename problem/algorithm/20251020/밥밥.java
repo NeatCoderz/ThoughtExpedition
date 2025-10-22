e1b5933066b407c01a7699fb0bda805aee0dfe1b
nimkoes <xxxelppa@gmail.com>
2025-10-22 22:51:33 +0900

    2025.10.22

---
+++ problem/algorithm/20251020/밥밥.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean solution(String[] phone_book) {
+        int size = phone_book.length;
+
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < size - 1; ++i) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
